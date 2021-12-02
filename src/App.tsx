e294dac9e6292b2af2da26204865d6a01afb4f06
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-01 21:01:27 -0800

    add the fetch method

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [dogsBreedName, setDogsBreedName] = useState([]);
+
+  const fetchData = () => {
+    console.log("start");
+
+    fetch(
+      "https://good-dogs.m90cinjkfob4u.us-west-2.cs.amazonlightsail.com/dogs",
+      {
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          // "Origin: http": "http://localhost:3000/",
+          // "Access-Control-Request-Method": "GET",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    )
+      .then((res) => {
+        console.log(res);
+
+        return res.json();
+      })
+
+      .then((data) => {
+        console.log("data", data);
+        setDogsBreedName(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        console.log("error");
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello</h2>
+      {dogsBreedName.map((dog, index) => {
+        <p key={index}>aaa</p>;
+      })}
     </div>
   );
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://good-dogs.m90cinjkfob4u.us-west-2.cs.amazonlightsail.com/",
+      target:
+        "https://good-dogs.m90cinjkfob4u.us-west-2.cs.amazonlightsail.com/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
